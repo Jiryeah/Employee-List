88d6a147c2961295bc95f503df8706b5c29a8ab3
Chidori <business.faissal@outlook.com>
2022-03-28 15:36:24 -0500

    finished employee tests

--- __tests__/Employeee.test.js
+++ __tests__/Employeee.test.js
@@ -22,3 +22,27 @@ test('Test names', () => {
   const newInstance = new Employee(testName);
   expect(newInstance.name).toBe(testName);
 });
+
+test('Can retrieve email w/ getEmail()', () => {
+  const testEmail = 'faissal5@testmail.com';
+  const newInstance = new Employee(testEmail);
+  expect(newInstance.getEmail()).toBe(testEmail);
+});
+
+test('Can retrieve id w/ getId()', () => {
+  const testId = 5;
+  const newInstance = new Employee(testId);
+  expect(newInstance.getId()).toBe(testId);
+});
+
+test('Can retrieve name w/ getName()', () => {
+  const testName = 'Faissal';
+  const newInstance = new Employee(testName);
+  expect(newInstance.getName()).toBe(testName);
+});
+
+test('getRole() returns Employee', () => {
+  const returnValue = 'Employee';
+  const newInstance = new Employee('Faissal', 5, 'faissal5@testmail.com');
+  expect(newInstance.getRole()).toBe(returnValue);
+});
