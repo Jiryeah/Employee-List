ad33e29e3c2ea1f1d7659ceb1143d5d9d5055847
Chidori <business.faissal@outlook.com>
2022-03-28 16:07:26 -0500

    Engineer test and all other tests completed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require('../lib/Engineer');
+
+test('Testing Github retrieval', () => {
+  const testHub = 'GitHub';
+  const newInstance = new Engineer('Test', 1, 'test1@testmail.com', testHub);
+  expect(newInstance.github).toBe(testHub);
+});
+
+test('Testing getGitHub()', () => {
+  const testHub = 'GitHub';
+  const newInstance = new Engineer('Test', 2, 'test2@testmail.com', testHub);
+  expect(newInstance.getGithub()).toBe(testHub);
+});
+
+test('Testing getRole()', () => {
+  const returnValue = 'Engineer';
+  const newInstance = new Engineer('Test', 3, 'test3@testmail.com', 'GitHub');
+  l;
+  expect(newInstance.getRole()).toBe(returnValue);
+});
