80e5fe7c81e781beae44a353be78d1181b9a75d2
Chidori <business.faissal@outlook.com>
2022-03-28 20:51:27 -0500

    template finished

--- src/template.js
+++ src/template.js
@@ -54,4 +54,61 @@ const renderTeam = (team) => {
   };
 
   // create an array for the html so that we can push new information dynamically
+  const pageHtml = [];
+
+  pageHtml.push(
+    team
+      .filter((employee) => employee.getRole() === 'Manager')
+      .map((manager) => renderManager(manager))
+  );
+  pageHtml.push(
+    team
+      .filter((employee) => employee.getRole() === 'Intern')
+      .map((intern) => renderIntern(intern))
+      .join('')
+  );
+  pageHtml.push(
+    team
+      .filter((employee) => employee.getRole() === 'Engineer')
+      .map((engineer) => renderEngineer(engineer))
+      .join('')
+  );
+
+  return pageHtml.join('');
+};
+
+module.exports = (team) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+
+    <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-heading">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="team-area col-12 d-flex justify-content-center">
+                  ${renderTeam(team)}
+              </div>
+          </div>
+      </div>
+    </body>
+  </html>
+  `;
 };
